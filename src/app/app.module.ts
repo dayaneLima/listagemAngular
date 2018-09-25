0132485cd96d1c1a192a73dfabedc68890a75666
Dayane <dayane.lima.castro@gmail.com>
2018-09-24 23:12:47 -0300

    adicionado angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { ContatosListagemComponent } from './contatos-listagem/contatos-listagem.component';
@@ -14,7 +17,10 @@ import { ContatoService } from './contato.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [ContatoService],
   bootstrap: [AppComponent]
